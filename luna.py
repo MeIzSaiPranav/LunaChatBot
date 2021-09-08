20c58a359a2881efaa976338cba3859c21b7bcf3
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-09-08 14:42:41 +0530

    update luna.py

--- luna.py
+++ luna.py
@@ -53,8 +53,8 @@ async def type_and_send(message):
 @luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
-        + " | [Group](t.me/PatheticProgrammers)",
+        "[Owner](t.me/Me_Iz_Mad_Boi)"
+        + " | [Group](t.me/PigasusSupport)",
         disable_web_page_preview=True,
     )
 
